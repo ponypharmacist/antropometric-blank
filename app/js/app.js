e824e13e92d76d80ca8c7a5850606c0c78d03e5e
Dmitry Glinskiy <glinskiy.dmitry@gmail.com>
2017-10-19 19:44:44 +0300

    Первая страница
    Первая страница, остальные как по маслу пойдут, если тут все ок

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,12 @@
 $(document).ready(function(){
   $('.element-remover').click(function(){
     var removeTarget = $(this).attr('id');
-    $('#is-' + removeTarget).hide();
+    $('.is-' + removeTarget).hide();
+  });
+
+  $('.copied-field').on('change', function() {
+    var copyTarget = $(this).attr('id');
+    $('.' + copyTarget + '-copy').html(this.value);
   });
 
 });
