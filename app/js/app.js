9714cbffb0f9cfa17ccd4ecc0aaebd2cf01dd1f4
Dmitry Glinskiy <glinskiy.dmitry@gmail.com>
2017-10-26 13:08:20 +0300

    WHR калькулятор
    Дополнительные поля не печатаются

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,29 @@
 // Made by Dmitry Glinskiy, contact me at glinskiy.net
 
+// Global variables
+// Patient general
+var ageYears = 0;
+var ageMonths = 0;
+var ageConverted = null;
+var genderSelected = null;
+
+// Patient Table 1
+var heightStanding = null;
+var heightStandingMeters = null;
+var heightSitting = null;
+var patientWeight = null;
+var patientBMI = null;
+var waistLength = null;
+var hipsLength = null;
+var patientWHR = null;
+var chestExcursion = null;
+var legLengthDifferenceRelative = null;
+var legLengthDifferenceAbsolute = null;
+
+// Patient Table 2
+
+
+// Helper functions
 function range(x, min, max) {
   return x >= min && x <= max;
 };
@@ -8,18 +32,27 @@ function getSumm(a,b) {
   return a + b;
 };
 
-// Global variables
-var ageYears = 0;
-var ageMonths = 0;
-var ageConverted = null;
-var genderSelected = null;
-
+//=========================================
+// Калькуляторы параметров пациента
+//=========================================
 function setAgeConverted (a,b) {
   ageConverted = ageYears * 12 + ageMonths;
   $('.age-converted').html(ageConverted);
 };
 
+function calculateBMI () {
+  patientBMI = patientWeight / (heightStandingMeters * heightStandingMeters);
+  $('#body-mass-index').val(patientBMI);
+};
 
+function calculateWHR () {
+  patientWHR = waistLength / hipsLength;
+  $('#waist-hip-ratio').val(patientWHR.toFixed(2));
+};
+
+//=========================================
+// Основные реакции на изменения полей
+//=========================================
 $(document).ready(function(){
   // Кнопки для скрытия элементов
   $('.element-remover').click(function(){
@@ -58,6 +91,29 @@ $(document).ready(function(){
       genderSelectedRus = 'Ошибочка!';
     }
     $('.patient-gender-copy').html(genderSelectedRus);
+  });
+
+  // Считаем Body Mass Index (BMI)
+  $('#patient-weight').on('change', function() {
+    patientWeight = parseInt(this.value);
+    patientWeight && heightStanding && calculateBMI();
+  });
+
+  $('#height-standing').on('change', function() {
+    heightStanding = parseInt(this.value);
+    heightStandingMeters = heightStanding / 100;
+    patientWeight && heightStanding && calculateBMI();
+  });
+
+  // Считаем Waist-Hip-Ratio (WHR)
+  $('#waist-length').on('change', function() {
+    waistLength = parseInt(this.value);
+    waistLength && hipsLength && calculateWHR();
+  });
+
+  $('#hips-length').on('change', function() {
+    hipsLength = parseInt(this.value);
+    waistLength && hipsLength && calculateWHR();
   });
 
 });
